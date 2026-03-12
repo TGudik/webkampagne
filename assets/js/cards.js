89443c0041160ca7088d78a19118c7e8b86c489d
TGudik <144559662+TGudik@users.noreply.github.com>
2026-03-12 10:50:20 +0100

    slider på cards bg + shake animation på switches

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -6,6 +6,14 @@ const htmlCard = document.querySelector(".card.HTML");
 const cssCard = document.querySelector(".card.CSS");
 const jsCard = document.querySelector(".card.JS");
 
+const switchWrap = document.querySelectorAll(".switch")
+
+const images = [
+  "../assets/images/sliderbillede1.jpg",
+  "../assets/images/sliderbillede2.jpg",
+  "../assets/images/sliderbillede3.jpg",
+];
+
 /* HTML SWITCH TOGGLE */
 
 htmlSwitch.addEventListener("change", () => {
@@ -42,5 +50,80 @@ cssSwitch.addEventListener("change", () => {
     htmlCard.classList.remove("styled");
     cssCard.classList.remove("styled");
     jsCard.classList.remove("styled");
+  }
+});
+
+/* JS SWITCH TOGGLE */
+const bg1 = document.querySelector(".bg1")
+const bg2 = document.querySelector(".bg2")
+
+let index = 0;
+let order = true
+
+bg1.style.backgroundImage = `url(${images[0]})`
+
+let interval = null
+
+function startSlider() {
+  if (!interval) {
+    interval = setInterval(slide, 3500)
+  }
+}
+
+function stopSlider() {
+  clearInterval(interval)
+  interval = null
+  bg1.style.backgroundImage = "none"
+  bg2.style.backgroundImage = "none"
+}
+
+function slide() {
+
+  index = (index + 1) % images.length;
+
+  if (order) {
+    bg2.style.backgroundImage = `url(${images[index]})`;
+    bg2.style.opacity = 1;
+    bg1.style.opacity = 0;
+  } else {
+    bg1.style.backgroundImage = `url(${images[index]})`;
+    bg1.style.opacity = 1;
+    bg2.style.opacity = 0;
+  }
+
+  order = !order;
+
+}
+
+startSlider()
+
+jsSwitch.addEventListener("change", () => {
+
+  if (!jsSwitch.checked) {
+    stopSlider()
+    htmlSwitch.disabled = true
+    cssSwitch.disabled = true
+  } else {
+    startSlider()
+    htmlSwitch.disabled = false
+    cssSwitch.disabled = false
+  }
+
+})
+
+switchWrap[0].addEventListener("click", () => {
+  if (htmlSwitch.disabled) {
+
+    switchWrap[0].classList.remove("shake")
+    switchWrap[0].offsetHeight
+    switchWrap[0].classList.add("shake")
+  }
+})
+switchWrap[1].addEventListener("click", () => {
+  if (cssSwitch.disabled) {
+
+    switchWrap[1].classList.remove("shake")
+    switchWrap[1].offsetHeight
+    switchWrap[1].classList.add("shake")
   }
 })
